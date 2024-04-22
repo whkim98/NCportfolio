12af8f0f5125617fa8e970db412670740cedf594
김우형 <hyeong7272@daum.net>
2024-04-22 15:30:17 +0900

    0422

--- untitled/js/content.js
+++ untitled/js/content.js
@@ -25,26 +25,7 @@ function showContent(contentId) {
 
     // content.js
 
-    function showContent(contentId) {
-        var contentElement = document.getElementById("content");
-        contentElement.innerHTML = ""; // 기존 내용 초기화
 
-        var text;
-        var textWidth;
-
-        if (contentId === 1) {
-            // Skills 버튼 클릭 시
-            text = "Skills";
-            contentElement.innerHTML = "<ul><li>Content 1 - Item 1</li><li>Content 1 - Item 2</li></ul>";
-        } else if (contentId === 2) {
-            // Educations 버튼 클릭 시
-            text = "Educations";
-            contentElement.innerHTML = "<ul><li>Content 2 - Item 1</li><li>Content 2 - Item 2</li></ul>";
-        } else if (contentId === 3) {
-            // Certifications 버튼 클릭 시
-            text = "Certifications";
-            contentElement.innerHTML = "<ul><li>Content 3 - Item 1</li><li>Content 3 - Item 2</li></ul>";
-        }
 
         textWidth = getTextWidth(text); // 텍스트의 길이를 가져옴
         barElement.style.width = textWidth + "px"; // 막대의 길이를 텍스트의 길이에 맞게 설정
@@ -55,7 +36,9 @@ function showContent(contentId) {
             var aboutSection = document.getElementById("about");
             aboutSection.scrollIntoView({ behavior: 'smooth' });
         }
-    }
+
 
 }
+
+document.querySelectorAll('button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
 
